c3ad842fae509b245446a261cab490f3c7d1d3d4
beyran11 <beyran11@gmail.com>
2020-05-17 18:25:20 +0430

    completed todo app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
+import SimpleTable from "./SimpleTable";
 
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, firstname: 'Hamzeh',lastname: 'Beiranvand',email: 'beyran11@gmail.com'},
+      {id: 2, firstname: 'Karim',lastname: 'Beiranvand',email: 'beiranvand.karim@gmail.com'}
+    ]
+  }
+
+  deleteTodo = (id) => {
+    //console.log(id);
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos
+    })
+  }
+
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos,todo]
+    this.setState({
+      todos
+    })
+  }
+
+
+  render() {
+    return (
+        <div className="todo-app container">
+          <h1 className="center blue-text">Sitoo App Todo's</h1>
+          <SimpleTable todos={this.state.todos} deleteTodo={this.deleteTodo} />
+
+
+        </div>
+    );
+  }
 }
 
 export default App;
